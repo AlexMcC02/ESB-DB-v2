cc7b9fd572eb17accf12df943520d3966b4012f3
Alex McCullough <alex.mccullough@kainos.com>
2025-08-21 11:06:05 +0100

    feat: proper searching

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,12 +1,27 @@
 function Search({ searchTerm, setSearchTerm }) {
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const inputValue = e.target.elements.searchInput.value;
+    setSearchTerm(inputValue);
+  };
+
   return (
-    <input
-      type="text"
-      placeholder="Enter parent star name... (e.g., Kepler-22)"
-      className="p-2 border-2 rounded-4xl w-150"
-      value={searchTerm}
-      onChange={(e) => setSearchTerm(e.target.value)}
-    />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="searchInput"
+        placeholder="Enter parent star name... (e.g., Kepler-22)"
+        className="p-2 border-2 rounded-4xl w-150"
+        defaultValue={searchTerm}
+      />
+      <button
+        type="submit"
+        className="border-2 rounded-4xl p-2 ml-2 bg-blue-500 text-white hover:bg-blue-700"
+      >
+        Submit
+      </button>
+    </form>
   );
 }
 
